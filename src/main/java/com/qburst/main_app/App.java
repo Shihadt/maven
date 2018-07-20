1767024d91c6fd556554d1a2aff7e0d501494fc8
shihadt <shihad@qburst.com>
2018-07-20 11:16:04 +0530

    first commit

---
+++ src/main/java/com/qburst/main_app/App.java
@@ -0,0 +1,16 @@
+package com.qburst.main_app;
+
+import com.qburst.my_app.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MyApp a = new MyApp();
+        a.someFunction();
+        System.out.println("Some other values");
+    }
+}
